2f413a9650590940e5c4641bb1560cdf27911702
Aaron Reed <aaron73@stanford.edu>
2019-08-24 23:29:05 -0700

    rotation test

--- servo_ex.py
+++ servo_ex.py
@@ -3,11 +3,12 @@ import RPi.GPIO as GPIO
 import time
 import sys
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.OUT)
-pwm = GPIO.PWM(18, 100)
-pwm.start(2.5)
-
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(18, GPIO.OUT)
+    pwm = GPIO.PWM(18, 100)
+    pwm.start(2.5)
+    
 
 dutyR = 15.5
 dutyL = 10.5
@@ -19,22 +20,37 @@ def rot(deg):
 		time.sleep(0.5)
 		pwm.ChangeDutyCycle(duty0)
 	
-class App:
-	
-    def __init__(self, master):
-        frame = Frame(master)
-        frame.pack()
-        scale = Scale(frame, from_=100, to=130, 
-              orient=HORIZONTAL, command=self.update)
-        scale.grid(row=0)
+# class App:
+# 	
+#     def __init__(self, master):
+#         frame = Frame(master)
+#         frame.pack()
+#         scale = Scale(frame, from_=100, to=130, 
+#               orient=HORIZONTAL, command=self.update)
+#         scale.grid(row=0)
+# 
+# 
+#     def update(self, angle):
+#         duty = float(angle) / 10.0 + 2.5
+#         pwm.ChangeDutyCycle(duty)
+# 
+# root = Tk()
+# root.wm_title('Servo Control')
+# app = App(root)
+# root.geometry("200x50+0+0")
+# root.mainloop()
 
+def main():
+    args = sys.argv[1:]
 
-    def update(self, angle):
-        duty = float(angle) / 10.0 + 2.5
-        pwm.ChangeDutyCycle(duty)
+    if not args:
+        print 'usage: angle'
+        sys.exit(1)
+    setup()
+    angle = args[0]
+    rot(angle)
+    exit(1)
 
-root = Tk()
-root.wm_title('Servo Control')
-app = App(root)
-root.geometry("200x50+0+0")
-root.mainloop()
+if __name__ == '__main__':
+    main()
+
